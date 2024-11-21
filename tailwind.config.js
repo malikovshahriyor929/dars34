e752196b3a400ca254e1b67ee2fb7c92a032ff43
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-11-21 14:25:43 +0500

    werwe

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        1: "#ff9846",
+        2: "#f2f2f2",
+        3: "#111",
+        4: "#a4acad",
+        5: "#f46d40",
+        7: "#f2f2f2",
+        8: "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
